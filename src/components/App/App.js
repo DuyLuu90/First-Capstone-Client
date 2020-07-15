135eb12ffd73289aaf64206533f47827d8548fcd
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-07-15 01:32:26 -0700

    add folders and files

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        THIS IS MY FIRST FULL STACK CAPSTONE
+      </h1>
+    </div>
+  );
+}
+
+export default App;
