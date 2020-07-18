64b7b30a22cf4e18428029eb039dad393830bdbb
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-07-17 23:50:52 -0700

    Updates

---
+++ precache-manifest.ee07b70144eb875705163a2d4c3e7e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af83ad719f2d10541029e2d6a3673fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7dee06384c73b23389",
+    "url": "/static/css/main.18427c6b.chunk.css"
+  },
+  {
+    "revision": "b9347d68735f025d1587",
+    "url": "/static/js/2.e852ac5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e852ac5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7dee06384c73b23389",
+    "url": "/static/js/main.7f59b941.chunk.js"
+  },
+  {
+    "revision": "64151c01024207a3ba3f",
+    "url": "/static/js/runtime-main.fb3b3580.js"
+  }
+]);
